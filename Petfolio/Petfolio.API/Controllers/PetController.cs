b6b72765c8b73bff7f6b57278bb9326a598185f8
Thyago Euclides <flooddk@gmail.com>
2025-11-17 10:36:13 -0400

    Implement pet deletion functionality
    Adding:
    - `Petfolio.Application/UseCases/Pets/Delete/DeletePetByIdUseCase.cs`: Implements the use case for deleting a pet by its ID.
    
    Updating:
    - `Petfolio.API/Controllers/PetController.cs`: Adds a new HTTP DELETE endpoint to delete a pet by its ID, utilizing the new delete use case.

--- Petfolio/Petfolio.API/Controllers/PetController.cs
+++ Petfolio/Petfolio.API/Controllers/PetController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Petfolio.Application.UseCases.Pets.Delete;
 using Petfolio.Application.UseCases.Pets.GetAll;
 using Petfolio.Application.UseCases.Pets.GetById;
 using Petfolio.Application.UseCases.Pets.Register;
@@ -69,5 +70,18 @@ public class PetController : ControllerBase
         var response = useCase.Execute(id);
 
         return Ok(response);
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status404NotFound)]
+    public IActionResult Delete(int id)
+    {
+        var useCase = new DeletePetByIdUseCase();
+
+        useCase.Execute(id);
+
+        return NoContent();
     }
 }
