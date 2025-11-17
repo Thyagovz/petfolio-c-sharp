0a447012179f94caf20c18ac0e33b6d1811dcaa7
Thyago Euclides <flooddk@gmail.com>
2025-11-17 09:11:35 -0400

    Add get all pets endpoint
    Adding:
    - Implements logic to retrieve all pets (Petfolio.Application/UseCases/Pets/GetAll/GetAllPetsUseCase.cs)
    - Defines the comprehensive response structure for all pets (Petfolio.Communication/Responses/ResponseAllPetJson.cs)
    - Defines a concise response structure for individual pets (Petfolio.Communication/Responses/ResponseShortPetJson.cs)
    
    Updating:
    - Integrates a new GetAll endpoint to retrieve pets (Petfolio.API/Controllers/PetController.cs)

--- Petfolio/Petfolio.API/Controllers/PetController.cs
+++ Petfolio/Petfolio.API/Controllers/PetController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Petfolio.Application.UseCases.Pets.GetAll;
 using Petfolio.Application.UseCases.Pets.Register;
 using Petfolio.Application.UseCases.Pets.Update;
 using Petfolio.Communication.Requests;
@@ -34,6 +35,23 @@ public class PetController : ControllerBase
 
         useCase.Execute(id, request);
 
+        return NoContent();
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(ResponseAllPetJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+
+    public IActionResult GetAll()
+    {
+        var useCase = new GetAllPetsUseCase();
+
+        var response = useCase.Execute();
+
+        if (response.Pets.Any())
+        {
+            return Ok(response);
+        }
         return NoContent();
     }
 }
