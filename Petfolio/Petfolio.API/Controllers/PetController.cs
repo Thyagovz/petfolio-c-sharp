e035991fbfc1128ffec5b2c07dbff24a534a5163
Thyago Euclides <flooddk@gmail.com>
2025-11-17 07:33:29 -0400

    Add Update endpoint and unify pet request model
    The PetController now includes a new Update endpoint (`HttpPut`) to handle updates to pet information. The endpoint uses the new `UpdatePetUseCase` class, which was added but currently lacks implementation.
    
    The request model for registering and updating pets was unified by renaming `RequestRegisterPetJson` to `RequestPetJson`. The `Register` method in `PetController` and `RegisterPetUseCase` were updated to use this new model.
    
    Namespaces were adjusted for consistency, changing `Pet` to `Pets` in the `RegisterPetUseCase` file. These changes improve maintainability and prepare the codebase for future enhancements.

--- Petfolio/Petfolio.API/Controllers/PetController.cs
+++ Petfolio/Petfolio.API/Controllers/PetController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Petfolio.Application.UseCases.Pet.Register;
+using Petfolio.Application.UseCases.Pets.Update;
+using Petfolio.Application.UseCases.Pets.Register;
 using Petfolio.Communication.Requests;
 using Petfolio.Communication.Responses;
+
 
 namespace Petfolio.API.Controllers;
 [Route("api/[controller]")]
@@ -10,12 +12,24 @@ public class PetController : ControllerBase
 {
     [HttpPost]
     [ProducesResponseType(typeof(ResponseRegisterPetJson), StatusCodes.Status201Created)]
-    public IActionResult Register([FromBody] RequestRegisterPetJson request)
+    public IActionResult Register([FromBody] RequestPetJson request)
     {
-        var useCase = new RegisterPetUseCase();
+        var useCase = new RegisterPetUseCase(); 
 
         var response = useCase.Execute(request);
 
         return Created(string.Empty, response);
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult Update([FromRoute] int id, [FromBody] RequestPetJson request)
+    {
+        var useCase = new UpdatePetUseCase();   
+
+        useCase.Execute(id, request);
+
+        return NoContent();
     }
 }
